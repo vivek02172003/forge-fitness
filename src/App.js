d078e201a07c92c069a5ba2d7c7d310b37482d4d
Vivek Bhandari <v_bhandari@outlook.com>
2023-07-13 07:45:38 -0400

    fix refresh error

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="/forge-fitness">
       <Navbar />
       <Routes>
         <Route path="/*" element={<HomePage />} />
