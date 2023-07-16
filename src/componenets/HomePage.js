b17052a0c7330a90110df1b8f9bf50dfc7258184
Vivek Bhandari <v_bhandari@outlook.com>
2023-07-16 18:48:42 -0400

    add translation to home page

--- src/componenets/HomePage.js
+++ src/componenets/HomePage.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './HomePage.css';  
 import GoogleFormButton from './GoogleFormButton';
 import { Link, useLocation } from 'react-router-dom';
+import { initReactI18next, useTranslation } from 'react-i18next';
 
 function HomePage() {
+  const { t } = useTranslation();
     return (
         <div className="home-page">
           <div className="background-image-home"></div>
           <div className="content">
             <h1 className="site-title">Forge Fitness</h1>
-            <Link to="membership"><button className="get-started-button">Get Started Today</button></Link>
+            <Link to="membership"><button className="get-started-button">{t("signUpToday")}</button></Link>
           </div>
           <GoogleFormButton />
         </div>
